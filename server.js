cf9bf00316310b850812b0962512ce37c24e9cbc
Mateusz Moniowski <mlynski123@gmail.com>
2021-05-31 10:45:01 +0200

    chnge port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express")
 var app = express()
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 app.use(express.static('static'))
 app.use(express.json())
 //nasłuch na określonym porcie
